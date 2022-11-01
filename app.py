3d280fa923732a5e64cab9c73ff9c8ffe576f5aa
Kartik Shah <kartik@dynamic-oit-vapornet100-10-9-75-210.princeton.edu>
2022-10-31 21:04:38 -0400

    profile works

--- app.py
+++ app.py
@@ -16,40 +16,49 @@ def home():
     return render_template('login.html')
 
 
-@app.route('/create')
+@app.route('/create', methods = ['GET', 'POST'])
 def create():
     username = auth.authenticate()
     if server.check_user(username) == -1:
+        if request.method == 'POST':
+            print('hi')
+            server.create_user(request.form, username)
         return render_template('create_account.html')
     else: 
-        return render_template('profile_page.html')
+        details = server.profile_details(username)
+        name = details[0][1]
+        year = details[0][3]
+        plan = details[0][4]
+        number = details[1][1]
+        return render_template('profile_page.html', name = name, netid = str(username), class_year = year, dining_plan = plan, phone_no = number)
 
 
 @app.route('/test')
 def test():
     return render_template('test.html')
 
+'''
 @app.route('/create-new-user', methods=['GET', 'POST'])
 def my_form_post():
-    if request.method == 'POST':
-        firstname = request.form['firstname']
-        result = {
-            "output": firstname
-        }
+   if request.method == 'POST':
+       firstname = request.form['firstname']
+       result = {
+           "output": firstname
+       }
 
-        server.create_user(request.form)
+       server.create_user(request.form, username)
 
         
     
         
-        # TODO: ADD TO DATABASE HERE
+        TODO: ADD TO DATABASE HERE
         
-        # TODO; Redirect the user instead of calling jsonify
+        TODO; Redirect the user instead of calling jsonify
         
-        return jsonify(result=result)
+       return jsonify(result=result)
 
-    return render_template('create_account.html')
-
+   return render_template('create_account.html')
+'''
 
 @app.route('/about')
 def about():
--- server.py
+++ server.py
@@ -3,12 +3,16 @@ import app
 import os
 import psycopg2
 
-def create_user(details):
-    netid = str(details['netid'])
+def create_user(details, netid):
+    netid = str(netid)
     year = str(details['year'])
     plan = str(details['plan'])
     user = 'user'
-    val = (netid, user, year, plan)
+    name = str(details['name'])
+    users = (netid, name, user, year, plan)
+    phone = str(details['number'])
+    email = netid + "@princeton.edu"
+    contact = (netid, phone, email)
 
     try:
         database_url = os.getenv('DATABASE_URL')
@@ -16,10 +20,11 @@ def create_user(details):
         with psycopg2.connect(database_url) as connection:
 
             with connection.cursor() as cursor:
-                cursor.execute("INSERT INTO users (netid, usertype, year, plan) "
-                    + "VALUES (%s, %s, %s, %s)", val)
+                cursor.execute("INSERT INTO users (netid, name, usertype, year, plan) "
+                    + "VALUES (%s, %s, %s, %s, %s)", users)
     
-
+                cursor.execute("INSERT INTO contact (netid, phone, email) "
+                    + "VALUES (%s, %s, %s)", contact)
     except Exception as ex:
         print(ex, file=sys.stderr)
         sys.exit(1)
@@ -39,6 +44,25 @@ def check_user(username):
                     return -1
                 else:
                     return 0
+
+    except Exception as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+
+def profile_details(username):
+
+    username = str(username)
+    try:
+        database_url = os.getenv('DATABASE_URL')
+
+        with psycopg2.connect(database_url) as connection:
+
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM users WHERE netid = %s",[username])
+                row = cursor.fetchone()
+                cursor.execute("SELECT * FROM contact WHERE netid = %s",[username])
+                row2 = cursor.fetchone()
+                return (row, row2)     
 
     except Exception as ex:
         print(ex, file=sys.stderr)
